76da09835f9a9ca0bdfe6ea2f04132d2d33903c7
Rui Wang <wangruirrrwww@gmail.com>
2023-02-05 17:23:56 +0800

    refactor exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { topic, sheet, root, builder } from './builder'
+export * from './builder'
+export * from './internal/marker'
 export { Workbook } from './internal/model/workbook'
+export { TopicAttributes, TopicImageData } from './internal/model/topic'
