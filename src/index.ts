4d003a774acf6a724c94cf64ca7767595b2f20ed
rui wang <wangruirrrwww@gmail.com>
2023-07-17 12:23:12 +1000

    Refactor: reexport internals

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-export {
-  root as Root,
-  topic as Topic,
-  relationship as Relationship,
-  summary as Summary,
-  generateWorkbook,
-  RelationshipInfo,
-  SummaryInfo,
-  TopicBuilder,
-  RootBuilder,
-  WorkbookBuilder,
-  WorkbookDocument
-} from './builder'
-export * from './internal/marker'
-export { TopicImageData } from './internal/model/topic'
+export * from './exports'
 export * as helper from './helper'
