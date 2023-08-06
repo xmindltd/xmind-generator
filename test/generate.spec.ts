84435e362de14bcb2a1a70a1d019492a6d76cd07
synix <fangheff@gmail.com>
2023-08-06 11:33:21 +0800

    fix: test/generate.spec.ts

--- test/generate.spec.ts
+++ test/generate.spec.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { existsSync } from 'fs'
 import { describe, it, expect } from 'vitest'
-import { Topic, Root, Relationship, Summary, generateWorkbook, Marker, helper } from '../src'
+import { Topic, Root, Relationship, Summary, generateWorkbook, Marker, helper } from '../'
 
 describe('write xmind file', () => {
   it('should write workbook to xmind file', async () => {
@@ -11,6 +11,7 @@ describe('write xmind file', () => {
         .image(image.data, image.type)
         .children([
           Topic('Salad')
+            .ref('topic:salad')
             .markers([Marker.Arrow.refresh])
             .children([
               Topic('Garden Salad')
@@ -28,10 +29,10 @@ describe('write xmind file', () => {
             ])
         ])
         .relationships([
-          Relationship('', { from: 'Salad', to: 'topic:bar' }),
-          Relationship('Special', { from: 'topic:fred', to: 'topic:thud' })
+          Relationship('', { from: { topic: 'Salad' }, to: { ref: 'topic:bar' } }),
+          Relationship('Special', { from:{ ref: 'topic:fred' }, to: { ref: 'topic:thud' } })
         ])
-        .summaries([Summary('Fresh and Delicious', { from: 'Salad', to: 'topic:bar' })])
+        .summaries([Summary('Fresh and Delicious', { from: 'topic:salad', to: 'topic:bar' })])
     )
     await helper.saveLocal(document, path.resolve(__dirname))
     expect(existsSync(path.resolve(__dirname, 'Grill House.xmind'))).toBe(true)
