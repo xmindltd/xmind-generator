b546eeec279c374b98920e8cbf8799fff96ee82b
synix <fangheff@gmail.com>
2023-12-05 17:53:19 +0800

    refactor: node-helper implementation

--- src/node-helper.ts
+++ src/node-helper.ts
@@ -1,10 +1,8 @@
-import { writeFile } from 'fs'
-import { readFile } from 'fs/promises'
-import path from 'path'
 import type { WorkbookBuilder } from './builder'
 import type { NamedResourceData } from './storage'
 
 export async function writeLocalFile(workbook: WorkbookBuilder, pathToFile: string) {
+  const { writeFile } = await import('fs')
   const buffer = await workbook.archive()
   writeFile(pathToFile, Buffer.from(buffer), err => {
     if (err) throw err
@@ -12,6 +10,8 @@ export async function writeLocalFile(workbook: WorkbookBuilder, pathToFile: stri
 }
 
 export async function readImageFile(filePath: string): Promise<NamedResourceData> {
+  const { readFile } = await import('fs/promises')
   const filerBuffer = await readFile(filePath)
+  const path = await import('path')
   return { data: filerBuffer, name: path.basename(filePath) }
 }
