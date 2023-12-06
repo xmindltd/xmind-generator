6e56c43d71132095e9e45c766576fa422f975b2e
synix <fangheff@gmail.com>
2023-12-06 09:13:02 +0800

    refactor: node-helper implementation (#25)
    * refactor: node-helper implementation
    
    * chore: add install tip in readme
    
    * refactor: write file using 'fs/promises' instead of 'fs'

--- src/node-helper.ts
+++ src/node-helper.ts
@@ -1,17 +1,15 @@
-import { writeFile } from 'fs'
-import { readFile } from 'fs/promises'
-import path from 'path'
 import type { WorkbookBuilder } from './builder'
 import type { NamedResourceData } from './storage'
 
 export async function writeLocalFile(workbook: WorkbookBuilder, pathToFile: string) {
+  const { writeFile } = await import('fs/promises')
   const buffer = await workbook.archive()
-  writeFile(pathToFile, Buffer.from(buffer), err => {
-    if (err) throw err
-  })
+  await writeFile(pathToFile, Buffer.from(buffer))
 }
 
 export async function readImageFile(filePath: string): Promise<NamedResourceData> {
+  const { readFile } = await import('fs/promises')
   const filerBuffer = await readFile(filePath)
+  const path = await import('path')
   return { data: filerBuffer, name: path.basename(filePath) }
 }
